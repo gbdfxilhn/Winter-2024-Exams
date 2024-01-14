4a410bdc0a95ef588c72fb89818e1fc5527ee8c1
gbdfxilhn <155917785+gbdfxilhn@users.noreply.github.com>
2024-01-14 13:58:31 -0800

    Giving meaningful names

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,14 +1,14 @@
 // Return an array without duplicates
-
-duplicate = (value, N) => {
-  if (N <= 0) return [];
+//Giving meaningful names
+arrayWithoutDuplicates = (value, numberOfRepeats) => {
+  if (numberOfRepeats <= 0) return [];
   else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
+    result = [];
+    for (let i = 0; i < numberOfRepeats; i++) {
+      result[i] = value;
     }
-    return res;
+    return result;
   }
 };
 
-module.exports = duplicate;
+module.exports = arrayWithoutDuplicates;
