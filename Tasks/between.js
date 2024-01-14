cfbba4c24398ca133d3e2fa16848cbe29839814a
gbdfxilhn <155917785+gbdfxilhn@users.noreply.github.com>
2024-01-13 23:35:04 -0800

    Formating code identations and removing blocks where it could be possible

--- Tasks/between.js
+++ Tasks/between.js
@@ -2,17 +2,14 @@
 
 const getValueBetween = (fullString, prefix, suffix) => {
   const index = fullString.indexOf(prefix);
-  if (index === -1) return '';
+  if (index === -1) return '';  
   else {
     let prefixLength = index + prefix.length;
     fullString = fullString.substring(prefixLength);
     if (suffix) {
       index = fullString.indexOf(suffix);
-      if (index === -1) {
-        return '';
-      } else {
-        fullString = fullString.substring(0, index);
-      }
+      if (index === -1)  return '';
+      else fullString = fullString.substring(0, index);
     }
   }
   return fullString;
