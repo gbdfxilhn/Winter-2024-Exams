d01f8f2707e3891d031df26e576d14f338b78eb8
gbdfxilhn <155917785+gbdfxilhn@users.noreply.github.com>
2024-01-14 13:01:20 -0800

    Removing senseless blocks

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,11 +3,7 @@
 function intersectionOfTwoDictionaries(firstObject, secondObject) {
   const keys = Object.keys(firstObject);
   for (const attributeName of keys) {
-    if (firstObject[attributeName] === secondObject[attributeName]) {
-      secondObject[attributeName] = firstObject[attributeName];
-    } 
-      
-    else {
+    if (firstObject[attributeName] !== secondObject[attributeName]) {
       delete firstObject[attributeName];
     } 
   }
