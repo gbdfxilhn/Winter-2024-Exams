a51ffe3f94cc268e05d65a972d484c894e486716
gbdfxilhn <155917785+gbdfxilhn@users.noreply.github.com>
2024-01-14 13:42:30 -0800

    Declaring arrow function and 'key' as const, 'sum' as let and giving meaningful names

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,13 +1,13 @@
 // Sum all number values in dict
-//Deleting empty blocks
-count = (obj) => {
-  sum = 0;
-  keys = Object.keys(obj);
+//Declaring arrow function and 'key' as const, 'sum' as let and giving meaningful names
+const sumOfNumbers = (object) => {
+  let sum = 0;
+  const keys = Object.keys(object);
   keys.forEach((key) => {
-    const value = obj[key];
+    const value = object[key];
     if (typeof value === 'number') sum += value;
   });
   return sum;
 };
 
-module.exports = count;
+module.exports = sumOfNumbers;
